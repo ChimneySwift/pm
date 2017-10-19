4a4f1e67d2e23c35ef4c6d2a1d9cf7ad280c2a96
Swift797 <pono797@gmail.com>
2017-10-19 18:10:22 +1000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,26 @@
+local recipient
+
+minetest.register_chatcommand("pm", {
+    description = "Sends a pm to a specified player. Input no parameters to check recipient.",
+    func = function(param)
+    if param == "" then
+        if recipient == nil then
+            minetest.display_chat_message(minetest.colorize("red", "[PM]") .. " You must first select a recipient with .pm_set.")
+        else
+            minetest.display_chat_message(minetest.colorize("#00ff00", "[PM]") .. " Selected recipient: " .. recipient)
+        end
+    else
+        minetest.run_server_chatcommand("msg", recipient .. " " .. param)
+    end
+end })
+
+minetest.register_chatcommand("pm_set", {
+    description = "Sets the recipient for messages sent with .pm. Recipient names are case sensitive.",
+    func = function(param)
+    if param == "" then
+        minetest.display_chat_message(minetest.colorize("red", "[PM]") .. " You must enter a recipient.")
+    else
+        recipient = param
+        minetest.display_chat_message(minetest.colorize("#00ff00", "[PM]") .. " Recipient set to: " .. recipient)
+    end
+end })
